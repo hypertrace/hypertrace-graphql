06fddc48dee711da55b1d741f28c4cf119741258
Adrian Cole <adrian.f.cole@gmail.com>
2020-08-17 15:28:46 +0800

    Locks Java to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,11 @@ plugins {
 
 subprojects {
   group = "org.hypertrace.core.graphql"
+
+  pluginManager.withPlugin("java") {
+    configure<JavaPluginExtension> {
+      sourceCompatibility = JavaVersion.VERSION_11
+      targetCompatibility = JavaVersion.VERSION_11
+    }
+  }
 }
