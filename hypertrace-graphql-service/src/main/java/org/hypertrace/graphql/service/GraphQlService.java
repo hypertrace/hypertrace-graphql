5429b392140223352b18e886def8bec471adcb5a
Ronak <ronak@traceable.ai>
2020-08-21 21:14:24 +0530

    refactor to detach server and handler

--- hypertrace-graphql-service/src/main/java/org/hypertrace/graphql/service/GraphQlService.java
+++ hypertrace-graphql-service/src/main/java/org/hypertrace/graphql/service/GraphQlService.java
@@ -1,10 +1,15 @@
 package org.hypertrace.graphql.service;
 
+import org.eclipse.jetty.server.Server;
 import org.hypertrace.core.serviceframework.PlatformService;
 import org.hypertrace.core.serviceframework.config.ConfigClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GraphQlService extends PlatformService {
+  private static final Logger LOG = LoggerFactory.getLogger(GraphQlService.class);
 
+  private Server server;
   private GraphQlServiceImpl graphQlServiceImpl;
 
   public GraphQlService(ConfigClient configClient) {
@@ -14,13 +19,48 @@ public class GraphQlService extends PlatformService {
   @Override
   protected void doInit() {
     graphQlServiceImpl = new GraphQlServiceImpl(this.getAppConfig());
+    server = new Server(graphQlServiceImpl.getGraphQlServiceConfig().getServicePort());
+    server.setHandler(graphQlServiceImpl.getContextHandler());
+    server.setStopAtShutdown(true);
   }
 
   @Override
-  protected void doStart() { graphQlServiceImpl.start(); }
+  protected void doStart() {
+    LOG.info("Starting service: {}", this.getServiceName());
+    try {
+      server.start();
+    } catch (Exception e) {
+      LOG.error("Failed to start service: {}", this.getServiceName());
+      throw new RuntimeException(e);
+    }
+
+    try {
+      server.join();
+    } catch (InterruptedException ie) {
+      Thread.currentThread().interrupt();
+      throw new RuntimeException(ie);
+    }
+  }
 
   @Override
-  protected void doStop() { graphQlServiceImpl.stop(); }
+  protected void doStop() {
+    LOG.info("Shutting down service: {}", this.getServiceName());
+
+    graphQlServiceImpl.shutdown();
+
+    while (!server.isStopped()) {
+      try {
+        server.stop();
+      } catch (Exception e) {
+        LOG.error("Failed to shutdown service: {}", this.getServiceName());
+        throw new RuntimeException(e);
+      }
+    }
+    try {
+      Thread.sleep(100);
+    } catch (InterruptedException ignore) {
+    }
+  }
 
   @Override
   public boolean healthCheck() {
@@ -29,6 +69,6 @@ public class GraphQlService extends PlatformService {
 
   @Override
   public String getServiceName() {
-    return graphQlServiceImpl.getServiceName();
+    return graphQlServiceImpl.getGraphQlServiceConfig().getServiceName();
   }
 }
--- hypertrace-graphql-service/src/main/java/org/hypertrace/graphql/service/GraphQlServiceImpl.java
+++ hypertrace-graphql-service/src/main/java/org/hypertrace/graphql/service/GraphQlServiceImpl.java
@@ -3,83 +3,48 @@ package org.hypertrace.graphql.service;
 import com.typesafe.config.Config;
 import java.util.EnumSet;
 import javax.servlet.DispatcherType;
-import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.hypertrace.core.graphql.spi.config.GraphQlServiceConfig;
 import org.hypertrace.graphql.impl.GraphQlFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class GraphQlServiceImpl {
-  private static final Logger LOG = LoggerFactory.getLogger(GraphQlServiceImpl.class);
-
   private GraphQlServiceConfig graphQlServiceConfig;
   private DefaultGraphQlServiceLifecycle serviceLifecycle;
-  private Server server;
+  private ServletContextHandler contextHandler;
 
   public GraphQlServiceImpl(Config appConfig) {
     this.graphQlServiceConfig = new DefaultGraphQlServiceConfig(appConfig);
     this.serviceLifecycle = new DefaultGraphQlServiceLifecycle();
-    this.server = new Server(this.graphQlServiceConfig.getServicePort());
 
-    ServletContextHandler context = new ServletContextHandler();
+    contextHandler = new ServletContextHandler();
     if (this.graphQlServiceConfig.isCorsEnabled()) {
-      context.addFilter(
+      contextHandler.addFilter(
               CrossOriginFilter.class,
               this.graphQlServiceConfig.getGraphqlUrlPath(),
               EnumSet.of(DispatcherType.REQUEST));
     }
 
-    context.addServlet(
+    contextHandler.addServlet(
             new ServletHolder(
                     new GraphQlServiceHttpServlet(
                             GraphQlFactory.build(this.graphQlServiceConfig, this.serviceLifecycle))),
             this.graphQlServiceConfig.getGraphqlUrlPath());
 
-    this.server.setHandler(context);
-    this.server.setStopAtShutdown(true);
   }
 
-  public void start() {
-    LOG.info("Starting service: {}", this.getServiceName());
-
-    try {
-      server.start();
-    } catch (Exception e) {
-      LOG.error("Failed to start service: {}", this.getServiceName());
-      throw new RuntimeException(e);
-    }
-
-    try {
-      server.join();
-    } catch (InterruptedException ie) {
-      Thread.currentThread().interrupt();
-      throw new RuntimeException(ie);
-    }
+  public GraphQlServiceConfig getGraphQlServiceConfig() {
+    return graphQlServiceConfig;
   }
 
-  public void stop() {
-    LOG.info("Shutting down service: {}", this.getServiceName());
-    if (this.serviceLifecycle != null) {
-      this.serviceLifecycle.shutdown();
-    }
-    while (!server.isStopped()) {
-      try {
-        server.stop();
-      } catch (Exception e) {
-        LOG.error("Failed to shutdown service: {}", this.getServiceName());
-        throw new RuntimeException(e);
-      }
-    }
-    try {
-      Thread.sleep(100);
-    } catch (InterruptedException ignore) {
-    }
+  public ServletContextHandler getContextHandler() {
+    return contextHandler;
   }
 
-  String getServiceName() {
-    return this.graphQlServiceConfig.getServiceName();
+  public void shutdown() {
+    if (serviceLifecycle != null) {
+      serviceLifecycle.shutdown();
+    }
   }
 }
