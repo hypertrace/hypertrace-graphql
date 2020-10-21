c8719b7ffe7300f9c546db1071503db3245f96cd
Aaron Steinfeld <aaron@traceable.ai>
2020-10-21 12:23:17 -0400

    chore: use gradle platforms to share versions

--- hypertrace-graphql-explorer-context/build.gradle.kts
+++ hypertrace-graphql-explorer-context/build.gradle.kts
@@ -2,13 +2,9 @@ plugins {
   `java-library`
 }
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_11
-}
-
 dependencies {
-  api("com.google.inject:guice:4.2.3")
-  api("io.reactivex.rxjava3:rxjava:3.0.2")
+  api("com.google.inject:guice")
+  api("io.reactivex.rxjava3:rxjava")
   implementation("org.hypertrace.core.graphql:hypertrace-core-graphql-attribute-store")
   implementation("org.hypertrace.core.graphql:hypertrace-core-graphql-common-schema")
   implementation(project(":hypertrace-graphql-explorer-schema"))
--- hypertrace-graphql-service/build.gradle.kts
+++ hypertrace-graphql-service/build.gradle.kts
@@ -5,20 +5,16 @@ plugins {
   id("org.hypertrace.docker-publish-plugin")
 }
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_11
-}
-
 dependencies {
-  implementation("com.typesafe:config:1.4.0")
-  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.15")
-  implementation("org.slf4j:slf4j-api:1.7.30")
+  implementation("com.typesafe:config")
+  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.16")
+  implementation("org.slf4j:slf4j-api")
 
   implementation("org.eclipse.jetty:jetty-server:9.4.30.v20200611")
   implementation("org.eclipse.jetty:jetty-servlet:9.4.30.v20200611")
   implementation("org.eclipse.jetty:jetty-servlets:9.4.30.v20200611")
 
-  implementation("com.graphql-java-kickstart:graphql-java-servlet:9.1.0")
+  implementation("com.graphql-java-kickstart:graphql-java-servlet")
   implementation(project(":hypertrace-graphql-impl"))
   implementation("org.hypertrace.core.graphql:hypertrace-core-graphql-spi")
 
