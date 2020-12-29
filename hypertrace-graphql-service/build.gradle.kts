878286bf12c1d6174e9a7921406a9a898aaaff31
Pavan Kumar Kolamuri <pavankumar@Pavans-MacBook-Pro.local>
2020-12-29 20:06:52 +0530

    Removed unwanted changes

--- hypertrace-graphql-service/build.gradle.kts
+++ hypertrace-graphql-service/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 dependencies {
   implementation("com.typesafe:config")
-  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.18")
+  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.16")
   implementation("org.slf4j:slf4j-api")
 
-  implementation("org.eclipse.jetty:jetty-server:9.4.35.v20201120")
-  implementation("org.eclipse.jetty:jetty-servlet:9.4.35.v20201120")
-  implementation("org.eclipse.jetty:jetty-servlets:9.4.35.v20201120")
+  implementation("org.eclipse.jetty:jetty-server:9.4.30.v20200611")
+  implementation("org.eclipse.jetty:jetty-servlet:9.4.30.v20200611")
+  implementation("org.eclipse.jetty:jetty-servlets:9.4.30.v20200611")
 
   implementation("com.graphql-java-kickstart:graphql-java-servlet")
   implementation(project(":hypertrace-graphql-impl"))
@@ -28,10 +28,3 @@ tasks.run<JavaExec> {
   jvmArgs = listOf("-Dservice.name=${project.name}")
 }
 
-hypertraceDocker {
-  defaultImage {
-    javaApplication {
-      port.set(23431)
-    }
-  }
-}
