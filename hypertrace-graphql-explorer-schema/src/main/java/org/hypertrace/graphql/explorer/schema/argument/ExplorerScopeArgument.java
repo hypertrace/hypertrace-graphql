fe62266cf79e096d0640fc8c629c175652c81425
Aaron Steinfeld <aaron@traceable.ai>
2021-01-16 07:29:14 -0500

    fix: switch typo in explorer scope arg 'string' -> 'scope'

--- hypertrace-graphql-explorer-schema/src/main/java/org/hypertrace/graphql/explorer/schema/argument/ExplorerScopeArgument.java
+++ hypertrace-graphql-explorer-schema/src/main/java/org/hypertrace/graphql/explorer/schema/argument/ExplorerScopeArgument.java
@@ -3,5 +3,5 @@ package org.hypertrace.graphql.explorer.schema.argument;
 import org.hypertrace.core.graphql.deserialization.PrimitiveArgument;
 
 public interface ExplorerScopeArgument extends PrimitiveArgument<String> {
-  String ARGUMENT_NAME = "string";
+  String ARGUMENT_NAME = "scope";
 }
