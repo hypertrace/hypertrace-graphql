319763c51e1451b8cda3dc83ad8f7160792ba677
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2021-02-01 18:03:14 -0800

    chore: update dependencies (#68)

--- hypertrace-graphql-platform/build.gradle.kts
+++ hypertrace-graphql-platform/build.gradle.kts
@@ -9,7 +9,7 @@ javaPlatform {
 dependencies {
   api(platform("org.hypertrace.core.graphql:hypertrace-core-graphql-platform"))
   constraints {
-    api("org.hypertrace.entity.service:entity-type-service-rx-client:0.2.0")
-    api("org.hypertrace.config.service:spaces-config-service-api:0.1.0")
+    api("org.hypertrace.entity.service:entity-type-service-rx-client:0.5.6")
+    api("org.hypertrace.config.service:spaces-config-service-api:0.1.1")
   }
 }
