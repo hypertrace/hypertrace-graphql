be8e6856222e05c6e7603b08fcfd89bb4fd3d9ab
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2021-09-16 07:51:34 -0700

    Update hypertrace-graphql-entity-schema/src/main/java/org/hypertrace/graphql/entity/request/DefaultEntityLabelRequestBuilder.java

--- hypertrace-graphql-entity-schema/src/main/java/org/hypertrace/graphql/entity/request/DefaultEntityLabelRequestBuilder.java
+++ hypertrace-graphql-entity-schema/src/main/java/org/hypertrace/graphql/entity/request/DefaultEntityLabelRequestBuilder.java
@@ -17,7 +17,7 @@ import org.hypertrace.core.graphql.utils.schema.GraphQlSelectionFinder;
 import org.hypertrace.core.graphql.utils.schema.SelectionQuery;
 import org.hypertrace.graphql.entity.schema.Entity;
 
-public class DefaultEntityLabelRequestBuilder implements EntityLabelRequestBuilder {
+class DefaultEntityLabelRequestBuilder implements EntityLabelRequestBuilder {
 
   private static final String LABELS_ATTRIBUTE_KEY = "labels";
 
