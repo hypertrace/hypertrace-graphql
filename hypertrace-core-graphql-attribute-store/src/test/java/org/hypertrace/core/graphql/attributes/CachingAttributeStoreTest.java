f060ccc5e6790280ed4a8bf4a7850c1309fae4bb
ankitchoudhary111 <35135474+ankitchoudhary111@users.noreply.github.com>
2021-09-24 11:36:47 +0530

    Filter out internal attributes using the internal flag (#78)
    * filtered out attributes using the internal flag in the getAll method
    
    * corrected mistake in if condition
    
    * filtering out internal attributes
    
    * snyk-scan fix
    
    * same version for netty libs
    
    Co-authored-by: Ankit Choudhary <ankitchoudhary@192.168.2.103>

--- hypertrace-core-graphql-attribute-store/src/test/java/org/hypertrace/core/graphql/attributes/CachingAttributeStoreTest.java
+++ hypertrace-core-graphql-attribute-store/src/test/java/org/hypertrace/core/graphql/attributes/CachingAttributeStoreTest.java
@@ -53,7 +53,11 @@ class CachingAttributeStoreTest {
     AttributeModel spanIdAttribute =
         DefaultAttributeModel.builder().scope("SPAN").key("id").build();
     AttributeMetadata spanIdResponse =
-        AttributeMetadata.newBuilder().setScopeString("SPAN").setKey("id").build();
+        AttributeMetadata.newBuilder()
+            .setScopeString("SPAN")
+            .setKey("id")
+            .setInternal(false)
+            .build();
 
     when(this.mockAttributeClient.get("SPAN", "id")).thenReturn(Single.just(spanIdResponse));
     when(this.mockAttributeModeTranslator.translate(spanIdResponse))
