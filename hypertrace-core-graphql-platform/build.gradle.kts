e7d83ae01f46a09da26160b895a4178bab259f58
Kshitiz Saxena <84707889+saxenakshitiz@users.noreply.github.com>
2021-11-11 23:30:09 +0530

    Upgrade grpc and grpc-utils version (#83)

--- hypertrace-core-graphql-platform/build.gradle.kts
+++ hypertrace-core-graphql-platform/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 dependencies {
   constraints {
 
-    api("org.hypertrace.core.grpcutils:grpc-context-utils:0.6.1")
-    api("org.hypertrace.core.grpcutils:grpc-client-utils:0.6.1")
-    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.6.1")
+    api("org.hypertrace.core.grpcutils:grpc-context-utils:0.6.2")
+    api("org.hypertrace.core.grpcutils:grpc-client-utils:0.6.2")
+    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.6.2")
     api("org.hypertrace.gateway.service:gateway-service-api:0.1.170")
     api("org.hypertrace.core.attribute.service:caching-attribute-service-client:0.13.6")
 
@@ -23,18 +23,21 @@ dependencies {
     api("com.typesafe:config:1.4.1")
     api("com.google.guava:guava:30.1-jre")
     api("com.graphql-java-kickstart:graphql-java-servlet:10.1.0")
-    api("io.grpc:grpc-api:1.40.0")
-    api("io.grpc:grpc-core:1.40.0")
-    api("io.grpc:grpc-stub:1.40.0")
-    api("io.grpc:grpc-context:1.40.0")
+    api("io.grpc:grpc-api:1.42.0")
+    api("io.grpc:grpc-core:1.42.0")
+    api("io.grpc:grpc-stub:1.42.0")
+    api("io.grpc:grpc-context:1.42.0")
     api("com.fasterxml.jackson.core:jackson-databind:2.12.3")
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.3")
     api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.12.3")
     api("org.apache.commons:commons-text:1.9")
     api("io.opentelemetry:opentelemetry-proto:1.1.0-alpha")
+    api("com.google.code.gson:gson:2.8.9") {
+      because("https://snyk.io/vuln/SNYK-JAVA-COMGOOGLECODEGSON-1730327")
+    }
 
     runtime("org.apache.logging.log4j:log4j-slf4j-impl:2.14.0")
-    runtime("io.grpc:grpc-netty:1.40.0")
+    runtime("io.grpc:grpc-netty:1.42.0")
     runtime("io.netty:netty-codec-http2:4.1.68.Final") {
       because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-1083991")
     }
