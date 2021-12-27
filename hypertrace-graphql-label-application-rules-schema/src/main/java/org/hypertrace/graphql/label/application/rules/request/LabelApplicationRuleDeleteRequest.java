3cddf2130b5bd42c5d65656c140a72bc132dc4db
ankitchoudhary111 <35135474+ankitchoudhary111@users.noreply.github.com>
2021-12-27 10:39:05 +0530

    GraphQL: Merge the label-application-rules-schema into the labels-schema (#124)

--- hypertrace-graphql-label-application-rules-schema/src/main/java/org/hypertrace/graphql/label/application/rules/request/LabelApplicationRuleDeleteRequest.java
+++
@@ -1,7 +0,0 @@
-package org.hypertrace.graphql.label.application.rules.request;
-
-import org.hypertrace.core.graphql.common.request.ContextualRequest;
-
-public interface LabelApplicationRuleDeleteRequest extends ContextualRequest {
-  String id();
-}
---
+++ hypertrace-graphql-labels-schema-api/src/main/java/org/hypertrace/graphql/label/schema/rule/UnaryCondition.java
@@ -0,0 +1,23 @@
+package org.hypertrace.graphql.label.schema.rule;
+
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+import graphql.annotations.annotationTypes.GraphQLNonNull;
+
+@GraphQLName(UnaryCondition.TYPE_NAME)
+public interface UnaryCondition {
+  String TYPE_NAME = "LabelApplicationUnaryCondition";
+
+  String OPERATOR_KEY = "operator";
+
+  @GraphQLName(Operator.TYPE_NAME)
+  enum Operator {
+    OPERATOR_EXISTS;
+    private static final String TYPE_NAME = "UnaryConditionOperator";
+  }
+
+  @GraphQLField
+  @GraphQLNonNull
+  @GraphQLName(OPERATOR_KEY)
+  Operator operator();
+}
