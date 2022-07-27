c87f34821aa4c173e81d87d4c96a76ce23142574
Aaron Steinfeld <aaron@traceable.ai>
2022-07-27 02:21:17 -0400

    refactor: use http framework

--- hypertrace-graphql-service/build.gradle.kts
+++ hypertrace-graphql-service/build.gradle.kts
@@ -10,9 +10,7 @@ dependencies {
   implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.21")
   implementation("org.slf4j:slf4j-api")
 
-  implementation("org.eclipse.jetty:jetty-server:9.4.42.v20210604")
-  implementation("org.eclipse.jetty:jetty-servlet:9.4.42.v20210604")
-  implementation("org.eclipse.jetty:jetty-servlets:9.4.42.v20210604")
+  implementation("org.hypertrace.core.serviceframework:platform-http-service-framework:0.2.0-SNAPSHOT")
 
   implementation("com.graphql-java-kickstart:graphql-java-servlet")
   implementation(project(":hypertrace-graphql-impl"))
@@ -21,6 +19,7 @@ dependencies {
   annotationProcessor("org.projectlombok:lombok")
   compileOnly("org.projectlombok:lombok")
 
+  runtimeOnly("io.grpc:grpc-netty")
   runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl")
 
   annotationProcessor(platform(project(":hypertrace-graphql-platform")))
