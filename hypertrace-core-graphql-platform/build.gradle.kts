914fb0d87828af6a69a1d88b30f6b0f7d45c71a7
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-09-13 16:22:20 -0400

    refactor: update jackson (#108)
    * refactor: update jackson
    
    * fix: data packages require version

--- hypertrace-core-graphql-platform/build.gradle.kts
+++ hypertrace-core-graphql-platform/build.gradle.kts
@@ -30,9 +30,10 @@ dependencies {
     api("com.google.guava:guava:31.1-jre")
     api("com.graphql-java-kickstart:graphql-java-servlet:10.1.0")
 
-    api("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.6")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.12.6")
+    api("com.fasterxml.jackson.core:jackson-databind:2.13.4")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.13.4")
+    api("com.fasterxml.jackson:jackson-bom:2.13.4")
     api("org.apache.commons:commons-text:1.9")
     api("io.opentelemetry:opentelemetry-proto:1.1.0-alpha")
 
