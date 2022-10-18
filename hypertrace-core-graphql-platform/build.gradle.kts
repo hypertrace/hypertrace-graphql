41cfb349e75c7c5bc06028c21e3b8b95653cdf28
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-10-17 21:25:33 -0400

    refactor: update grpc utils (#116)
    * refactor: update grpc utils
    
    * refactor: update commons text too

--- hypertrace-core-graphql-platform/build.gradle.kts
+++ hypertrace-core-graphql-platform/build.gradle.kts
@@ -10,10 +10,10 @@ dependencies {
   api(platform("io.grpc:grpc-bom:1.47.0"))
   constraints {
 
-    api("org.hypertrace.core.grpcutils:grpc-context-utils:0.8.0")
-    api("org.hypertrace.core.grpcutils:grpc-client-utils:0.8.0")
-    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.8.0")
-    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.8.0")
+    api("org.hypertrace.core.grpcutils:grpc-context-utils:0.8.2")
+    api("org.hypertrace.core.grpcutils:grpc-client-utils:0.8.2")
+    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.8.2")
+    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.8.2")
     api("org.hypertrace.gateway.service:gateway-service-api:0.2.15")
     api("org.hypertrace.core.attribute.service:caching-attribute-service-client:0.14.8")
 
@@ -23,7 +23,6 @@ dependencies {
     api("org.slf4j:slf4j-api:1.7.36")
     api("io.reactivex.rxjava3:rxjava:3.1.5")
     api("com.google.protobuf:protobuf-java-util:3.21.7")
-    api("com.google.protobuf:protobuf-java:3.21.7")
 
     api("org.projectlombok:lombok:1.18.24")
     api("com.google.code.findbugs:jsr305:3.0.2")
@@ -35,7 +34,7 @@ dependencies {
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
     api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.13.4")
     api("com.fasterxml.jackson:jackson-bom:2.13.4")
-    api("org.apache.commons:commons-text:1.9")
+    api("org.apache.commons:commons-text:1.10.0")
     api("io.opentelemetry:opentelemetry-proto:1.1.0-alpha")
 
     runtime("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
