eb6dc3ed32c1b1f508a59f3e456a073ad20576ac
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2023-06-18 22:37:34 -0700

    refactor: update vulnerabilities (#136)
    * refactor: update vulnerabilities
    
    * fix: move bom declaration

--- hypertrace-core-graphql-platform/build.gradle.kts
+++ hypertrace-core-graphql-platform/build.gradle.kts
@@ -9,37 +9,37 @@ javaPlatform {
 val attributeServiceVersion: String = "0.14.14"
 
 dependencies {
-  api(platform("io.grpc:grpc-bom:1.47.0"))
+  api(platform("io.grpc:grpc-bom:1.56.0"))
+  api(platform("com.fasterxml.jackson:jackson-bom:2.15.2"))
   constraints {
 
-    api("org.hypertrace.core.grpcutils:grpc-context-utils:0.11.2")
-    api("org.hypertrace.core.grpcutils:grpc-client-utils:0.11.2")
-    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.11.2")
-    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.11.2")
+    api("org.hypertrace.core.grpcutils:grpc-context-utils:0.12.0")
+    api("org.hypertrace.core.grpcutils:grpc-client-utils:0.12.0")
+    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.12.0")
+    api("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.12.0")
     api("org.hypertrace.gateway.service:gateway-service-api:0.2.25")
     api("org.hypertrace.core.attribute.service:caching-attribute-service-client:${attributeServiceVersion}")
     api("org.hypertrace.core.attribute.service:attribute-service-api:${attributeServiceVersion}")
 
     api("com.google.inject:guice:5.1.0")
-    api("com.graphql-java:graphql-java:19.4")
+    api("com.graphql-java:graphql-java:20.4")
     api("io.github.graphql-java:graphql-java-annotations:9.1")
     api("org.slf4j:slf4j-api:1.7.36")
-    api("io.reactivex.rxjava3:rxjava:3.1.5")
+    api("io.reactivex.rxjava3:rxjava:3.1.6")
     api("com.google.protobuf:protobuf-java-util:3.21.7")
 
     api("org.projectlombok:lombok:1.18.24")
     api("com.google.code.findbugs:jsr305:3.0.2")
     api("com.typesafe:config:1.4.2")
-    api("com.google.guava:guava:31.1-jre")
+    api("com.google.guava:guava:32.0.1-jre")
     api("com.graphql-java-kickstart:graphql-java-servlet:14.0.0")
 
-    api("com.fasterxml.jackson.core:jackson-databind:2.13.4")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.13.4")
-    api("com.fasterxml.jackson:jackson-bom:2.14.2")
+    api("com.fasterxml.jackson.core:jackson-databind")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
     api("org.apache.commons:commons-text:1.10.0")
     api("io.opentelemetry:opentelemetry-proto:1.1.0-alpha")
 
-    runtime("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
+    runtime("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
   }
 }
